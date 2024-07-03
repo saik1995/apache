9a4f59f6d1bb9a37476f84c14f497932cdb99358
forpuppet1 <saikumarsaaya@gmail.com>
2024-07-03 20:41:09 +0530

    its done

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-# This class installs and configures the Apache HTTP server.
+# This class installs and configures the Apache HTTP server and sets up a virtual host.
 class apache {
   package { 'httpd':
     ensure => installed,
@@ -13,5 +13,25 @@ class apache {
   file { '/var/www/html/index.html':
     ensure  => file,
     content => 'Hello, Puppet!',
+  }
+
+  # Virtual Host Configuration
+  file { '/etc/httpd/conf.d/vhost.conf':
+    ensure  => file,
+    content => template('apache/vhost.conf.erb'),
+    require => Package['httpd'],
+  }
+
+  # Create the directory for the virtual host
+  file { '/var/www/vhosts/example':
+    ensure  => directory,
+    require => Package['httpd'],
+  }
+
+  # Example content for the virtual host
+  file { '/var/www/vhosts/example/index.html':
+    ensure  => file,
+    content => 'Welcome to the Example Virtual Host!',
+    require => File['/var/www/vhosts/example'],
   }
 }
